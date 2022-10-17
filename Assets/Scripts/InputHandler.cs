3a21e041807090d84af08a4234cb29f0c698fa2c
Wrath-Czesia <yann.berger@epitech.eu>
2022-10-17 17:29:44 +0200

    Camera movement

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -2,46 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputHandler : MonoBehaviour
+namespace OP
 {
-    public float horizontal;
-    public float vertical;
-    public float moveAmount;
-    public float mouseX;
-    public float mouseY;
-
-    PlayerControls inputActions;
-
-    Vector2 movementInput;
-    Vector2 cameraInput;
-
-    public void OnEnable()
+    public class InputHandler : MonoBehaviour
     {
-        if (inputActions == null)
+        public float horizontal;
+        public float vertical;
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
+
+        PlayerControls inputActions;
+        CameraHandler cameraHandler;
+
+        Vector2 movementInput;
+        Vector2 cameraInput;
+
+        private void Awake()
         {
-            inputActions = new PlayerControls();
-            inputActions.PlayerMovement.Movement.performed += inputActions => movementInput = inputActions.ReadValue<Vector2>();
-            inputActions.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
+            cameraHandler = CameraHandler.singleton;
         }
-        inputActions.Enable();
-    }
 
-    private void OnDisable()
-    {
-        inputActions.Disable();
-    }
+        private void FixedUpdate()
+        {
+            float delta = Time.fixedDeltaTime;
 
-    public void TickInput(float delta)
-    {
-        MoveInput(delta);
-    }
+            if (cameraHandler != null)
+            {
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandleCameraRotation(delta, mouseX, mouseY);
+            }
+        }
 
-    private void MoveInput(float delta)
-    {
-        horizontal = movementInput.x;
-        vertical = movementInput.y;
-        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
-        mouseX = cameraInput.x;
-        mouseY = cameraInput.y;
+        public void OnEnable()
+        {
+            if (inputActions == null)
+            {
+                inputActions = new PlayerControls();
+                inputActions.PlayerMovement.Movement.performed += inputActions => movementInput = inputActions.ReadValue<Vector2>();
+                inputActions.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
+            }
+            inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+        public void TickInput(float delta)
+        {
+            MoveInput(delta);
+        }
+
+        private void MoveInput(float delta)
+        {
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+            mouseX = cameraInput.x;
+            mouseY = cameraInput.y;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SG {
+namespace OP {
     public class PlayerController : MonoBehaviour
     {
         Transform cameraObject;
